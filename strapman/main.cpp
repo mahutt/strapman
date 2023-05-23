4680da4686b3829adde8b4e89c3dfb14bd75ac5d
mahutt <tommy.mahut@gmail.com>
2023-05-23 08:25:40 -0700

    Add project files.

---
+++ strapman/main.cpp
@@ -0,0 +1,27 @@
+#include <SFML/Graphics.hpp>
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(1000, 1000), "SFML works!");
+    sf::CircleShape shape(50.f);
+    shape.setOrigin(sf::Vector2f(50.f, 50.f));
+    shape.setPosition(500.f, 500.f);
+    shape.setFillColor(sf::Color::Yellow);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+                window.close();
+
+        }
+
+        window.clear();
+        window.draw(shape);
+        window.display();
+    }
+
+    return 0;
+}
